4667d931de92d3d5e086e2bdfbc43939ad371483
lujunlj <434998516@qq.com>
2019-06-28 00:51:35 +0800

    Initial Commit

---
+++ src/main/java/com/lujun/demo/ApiController.java
@@ -0,0 +1,20 @@
+package com.lujun.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created with IDEA
+ * author: lujun
+ * Date:2019/6/20
+ * Time:23:33
+ */
+@RestController
+public class ApiController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "hello SpringBoot!";
+    }
+
+}
